97641664057d136bd83b6bec54dd1fdbc6b8eae7
Antony Njoroge Mutharia <antonynjoro@gmail.com>
2023-06-29 21:08:15 -0600

    possible solution to the blank bug

--- celery_app.py
+++ celery_app.py
@@ -1,15 +1,11 @@
 import os
 from celery import Celery
-from celery_worker_functions import generate_resume
+
 
 
 BROKER_URL = os.environ.get('CLOUDAMQP_URL', 'pyamqp://guest@localhost//')
 celery_app = Celery('tasks', broker=BROKER_URL)
 
-
-# If your tasks are not auto-discovered, you might need to manually register them:
-
-celery_app.tasks.register(generate_resume)
 
 
 
